4648404566c7c0cd61297cd4a59216e164169713
Zak <zak.hajjaji@icloud.com>
2025-06-03 12:52:46 +0100

    Fix case sensitivity for AdController import

--- routes/adRoutes.js
+++ routes/adRoutes.js
@@ -1,17 +1,20 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const { getAds, addAd , updateAd, removeAd} = require('../controllers/AdController');
+const {
+  getAds,
+  addAd,
+  updateAd,
+  removeAd,
+} = require("../controllers/adController");
 
 // Get all ads
-router.get('/', getAds);
+router.get("/", getAds);
 
 // Create new ad
-router.post('/', addAd);
-
+router.post("/", addAd);
 
 router.put("/:id", updateAd);
 
 router.delete("/:id", removeAd);
-
 
 module.exports = router;
